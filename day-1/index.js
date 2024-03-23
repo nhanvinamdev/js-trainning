2c663b503abb53ffa79c7cfb3e5932c60cd01a4c
Nam Nhan <nhanvinam.dev@gmail.com>
2024-03-23 18:54:21 +0700

    day 1

---
+++ day-1/index.js
@@ -0,0 +1,22 @@
+// Khai báo biến
+// var + tên biến
+
+var total;
+
+// gán giá trị cho 1 biến
+var a = 1;
+
+// Các kiểu dữ liệu chính trong js
+
+// kiểu tham trị (value type)
+// string
+// number
+// NaN
+// undefined
+// null
+// Symbol
+
+// Kiểu tham chiếu (Refenrence type)
+// array
+// object
+// function
