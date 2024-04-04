fb00ce52042501ebcafd4d03056c668de532a1ba
Nam Nhan <nhanvinam.dev@gmail.com>
2024-04-04 20:47:08 +0700

    exam day 5

---
+++ day-5/exam.js
@@ -0,0 +1,4 @@
+// Cho 3 số a, b, c. Viết hàm tính + - * / của 3 con số này
+// Cho 2 số a, b. Viết hàm in ra "a lớn hơn b" nếu a > b và ngược lại
+// Viết hàm kiểm tra 1 số bất kì là số chẵn hay số lẽ, hàm trả ra 1 nếu là số chẵn, trả ra 0 nếu là số lẻ
+// Viết hàm tính chu vi hình tròn và diện tính hình tròn
