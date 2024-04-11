7a756875be568d7d158c185b612ad1c0b3ca1cea
Nam Nhan <nhanvinam.dev@gmail.com>
2024-04-11 10:05:24 +0700

    additioal exam

--- day-8/exam.js
+++ day-8/exam.js
@@ -5,12 +5,14 @@
 // 2. In các số lẽ từ 1 -> n
 // 3. tính tổng từ 1 đến số n đó (S = 1 + 2 + 3 + ... + n)
 // 4. tính tích từ 1 đến số n đó (S = 1.2.3.4.5. ... .n)
-// 5. Tính số lượng phần tử trong dãy số (12345 -> 5)
-// 6. Tính tổng số lượng phần tử trong dãy số (1234 -> 10)
-// 7. Bảng cữu chương 1 đến 9
-// 8. Tính giả trị của biểu thức S = 1.2 + 2.3 + 3.4 + ... + n( n + 1 )
-// 9. Viêt hàm truyền vào 2 số a và b, Tìm tổng tất cả các số chẵn và lẻ nằm giữa
+// 5. In ra màn hình giá trị của S = 1^2 + 2^2 + 3^2 + ... + n^2.
+// 6. In ra màn hình các số chia hết cho 5 từ 1 đến n. (VD: n = 20 -> 5 10 15 20)
+// 7. Tính số lượng phần tử trong dãy số (12345 -> 5)
+// 8. Tính tổng số lượng phần tử trong dãy số (1234 -> 10)
+// 9. Bảng cữu chương 1 đến 9
+// 10. Tính giả trị của biểu thức S = 1.2 + 2.3 + 3.4 + ... + n( n + 1 )
+// 11. Viêt hàm truyền vào 2 số a và b, Tìm tổng tất cả các số chẵn và lẻ nằm giữa
 // hai số đã được nhập (a = 5, b = 9 => tổng số lẻ = 21, tổng số chẵn = 14)
 
-// 10. In bảng cửu chương rút gọn: Bảng cửu chương rút gọn là bảng có hàng là kết quả của phép nhân một số với các giá trị từ 1 tới 10. Chúng ta sẽ in 9 hàng tương ứng với các số từ 2 tới 10.
-// 11. Vẽ tam giác đều xuôi và ngược
+// 12. In bảng cửu chương rút gọn: Bảng cửu chương rút gọn là bảng có hàng là kết quả của phép nhân một số với các giá trị từ 1 tới 10. Chúng ta sẽ in 9 hàng tương ứng với các số từ 2 tới 10.
+// 13. Vẽ tam giác đều xuôi và ngược
