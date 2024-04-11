6bdf361fedffebdaa796536f19d3ab1704374b66
Nam Nhan <nhanvinam.dev@gmail.com>
2024-04-11 09:50:08 +0700

    additioal exam

--- day-8/exam.js
+++ day-8/exam.js
@@ -1,11 +1,16 @@
 // BASIC
 
 // Viết hàm truyền vào tham số n:
-// tính tổng từ 1 đến số n đó (S = 1 + 2 + 3 + ... + n)
-// tính tích từ 1 đến số n đó
-// Tính số lượng phần tử trong dãy số (12345 -> 5)
-// Tính tổng số lượng phần tử trong dãy số (1234 -> 10)
-// Bảng cữu chương 1 đến 9
-// Tính giả trị của biểu thức S = 1.2 + 2.3 + 3.4 + ... + n(n+1)
-// Viêt hàm truyền vào 2 số a và b, Tìm tổng tất cả các số chẵn và lẻ nằm giữa
-// 2 số đã được nhập (a = 5, b = 9 => tổng số lẻ = 21, tổng số chẵn = 14)
+// 1. In các số chẵn từ 1 -> n
+// 2. In các số lẽ từ 1 -> n
+// 3. tính tổng từ 1 đến số n đó (S = 1 + 2 + 3 + ... + n)
+// 4. tính tích từ 1 đến số n đó (S = 1.2.3.4.5. ... .n)
+// 5. Tính số lượng phần tử trong dãy số (12345 -> 5)
+// 6. Tính tổng số lượng phần tử trong dãy số (1234 -> 10)
+// 7. Bảng cữu chương 1 đến 9
+// 8. Tính giả trị của biểu thức S = 1.2 + 2.3 + 3.4 + ... + n( n + 1 )
+// 9. Viêt hàm truyền vào 2 số a và b, Tìm tổng tất cả các số chẵn và lẻ nằm giữa
+// hai số đã được nhập (a = 5, b = 9 => tổng số lẻ = 21, tổng số chẵn = 14)
+
+// 10. In bảng cửu chương rút gọn: Bảng cửu chương rút gọn là bảng có hàng là kết quả của phép nhân một số với các giá trị từ 1 tới 10. Chúng ta sẽ in 9 hàng tương ứng với các số từ 2 tới 10.
+// 11. Vẽ tam giác đều xuôi và ngược
