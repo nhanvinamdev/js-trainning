daf7f0e29ed0b44be876a04feab2bd2a7203f9b4
Nam Nhan <nhanvinam.dev@gmail.com>
2024-04-13 17:09:33 +0700

    push

--- day-8/exam.js
+++ day-8/exam.js
@@ -2,9 +2,52 @@
 
 // Viết hàm truyền vào tham số n:
 // 1. In các số chẵn từ 1 -> n
+function bT1(n) {
+  //   for (let i = 2; i <= n; i = i + 2) {
+  //     // if (i % 2 === 0) console.log(i);
+  //     console.log(i);
+  //   }
+  let i = 2;
+  while (i <= n) {
+    console.log(i);
+    i = i + 2;
+  }
+}
+
+// console.log();
+// bT1(10);
+
 // 2. In các số lẽ từ 1 -> n
+function bT2(n) {
+  for (let i = 1; i <= n; i = i + 2) console.log(i);
+}
+// bT2(11);
 // 3. tính tổng từ 1 đến số n đó (S = 1 + 2 + 3 + ... + n)
+function bT3(n) {
+  let i = 1;
+  let sum = 0;
+  //   for (; i <= n; i++) {
+  //     sum = sum + i;
+  //   }
+  while (i <= n) {
+    sum = sum + i;
+    i++;
+  }
+  return sum;
+}
+// console.log(bT3(3));
+
 // 4. tính tích từ 1 đến số n đó (S = 1.2.3.4.5. ... .n)
+// function bT4(n) {
+//   let i = 1;
+//   let m = 1;
+//   for (; i <= n; i++) {
+//     m = m * i;
+//   }
+//   return m;
+// }
+// console.log(bT4(4));
+
 // 5. In ra màn hình giá trị của S = 1^2 + 2^2 + 3^2 + ... + n^2.
 // 6. In ra màn hình các số chia hết cho 5 từ 1 đến n. (VD: n = 20 -> 5 10 15 20)
 // 7. Tính số lượng phần tử trong dãy số (12345 -> 5)
