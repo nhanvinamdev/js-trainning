3054a85a8b09f09e44a744f153be53838c4cd394
TTyNHu <nhutran.ttyn@gmail.com>
2024-04-14 22:44:38 +0700

    day-8

--- day-8/exam.js
+++ day-8/exam.js
@@ -72,79 +72,101 @@
 //   bT6(100);
 
 // 7. Tính số lượng phần tử trong dãy số (12345 -> 5)
-function bT7(n)
-{
-    let c = Math.abs(n);
-    let i=0;
-    for ( ; c > 0 ; c = Math.floor(c / 10))
-    {
-    i++;}
-return i;
-}
-console.log(bT7(10));
+// function bT7(n)
+// {
+//     let c = Math.abs(n);
+//     let i=0;
+//     for ( ; c > 0 ; c = Math.floor(c / 10))
+//     {
+//     i++;}
+// return i;
+// }
+// console.log(bT7(10));
 
 
-var c= Math.floor(123 / 10)
-console.log(Math.floor(c / 10))
-// 8. Tính tổng số lượng phần tử trong dãy số (1234 -> 10)
-function bT8(n)
-{
-    let c = Math.abs(n);
-    let sum = 0;
-    let i=0;
-    for ( ; c > 0 ; c = Math.floor(c / 10))
-        {
-        sum = sum + (c % 10)
-    }
-return sum;
-}
-console.log(bT8(127000));
+// var c= Math.floor(123 / 10)
+// console.log(Math.floor(c / 10))
+// // 8. Tính tổng số lượng phần tử trong dãy số (1234 -> 10)
+// function bT8(n)
+// {
+//     let c = Math.abs(n);
+//     let sum = 0;
+//     let i=0;
+//     for ( ; c > 0 ; c = Math.floor(c / 10))
+//         {
+//         sum = sum + (c % 10)
+//     }
+// return sum;
+// }
+// console.log(bT8(127000));
 
-// 9. Bảng cữu chương 1 đến 9
-function bT9(n) {
-  let i = 1;
-  for (; i <= 10 && n>=1 && n<=9; i++)
-  {
-    console.log(`${n} x ${i} = ${n * i}`);
-  }
-  console.log(" nhap so tu 1 den 9");
-}
+// // 9. Bảng cữu chương 1 đến 9
+// function bT9(n) {
+//   let i = 1;
+//   for (; i <= 10 && n>=1 && n<=9; i++)
+//   {
+//     console.log(`${n} x ${i} = ${n * i}`);
+//   }
+//   console.log(" nhap so tu 1 den 9");
+// }
 
-bT9(11);
+// bT9(11);
 
-// // 10. Tính giả trị của biểu thức S = 1.2 + 2.3 + 3.4 + ... + n( n + 1 )
-function bT10(n) {
-      let i = 1;
-      let S = 0;
-      for (; i <= n; i++) {
-        S+= i*(i+1)
-      }
-      return S;
-    }
-    console.log(bT10(2));
+// // // 10. Tính giả trị của biểu thức S = 1.2 + 2.3 + 3.4 + ... + n( n + 1 )
+// function bT10(n) {
+//       let i = 1;
+//       let S = 0;
+//       for (; i <= n; i++) {
+//         S+= i*(i+1)
+//       }
+//       return S;
+//     }
+//     console.log(bT10(2));
 
 // // 11. Viêt hàm truyền vào 2 số a và b, Tìm tổng tất cả các số chẵn và lẻ nằm giữa
 // // hai số đã được nhập (a = 5, b = 9 => tổng số lẻ = 21, tổng số chẵn = 14)
 
-// // 12. In bảng cửu chương rút gọn: Bảng cửu chương rút gọn là bảng có hàng là kết quả của phép nhân một số với các giá trị từ 1 tới 10. Chúng ta sẽ in 9 hàng tương ứng với các số từ 2 tới 10.
+// function bt11(a,b) {
+//       let i = a;
+//       let sumLe = 0;
+//       let sumChan = 0;
+
+//       for (; i <= b; i++) {
+//         if (i % 2 !== 0)
+//         {
+//         sumLe= sumLe + i}
+//         else sumChan = sumChan + i;
+
+//     }
+//     console.log("Tong so le",sumLe);
+//     console.log("Tong so chan",sumChan);
+// }
+//     bt11(5, 9)
+//1 2 3 4 5 6 7 8 9 10 11
+// // 12. In bảng cửu chương rút gọn: Bảng cửu chương rút gọn là bảng
+// có hàng là kết quả của phép nhân một số với các giá trị từ 1 tới 10.
+// Chúng ta sẽ in 9 hàng tương ứng với các số từ 2 tới 10.
+////
+
 
 // // MAP, FILTER, FIND
 // /**
 //  * Input: numbers = [1, 2, 3, 4, 5];
 //  * Output : _numbers = [2, 4 , 6, 8, 10]
 //  *
+
 //  * Input: const strings = ["hello", "world", "javascript"];
 //  * Output: ["HELLO", "WORLD", "JAVASCRIPT"]
 //  *
 //  * Input: const objects = [{ name: "John" }, { name: "Jane" }, { name: "Peter" }];
 //  * Output: [{ id: 1, name: "John" }, { id: 2, name: "Jane" }, { id: 3, name: "Peter" }]
 //  *
-//  * Input: const products = [{ id: 1, name: "Laptop", price: 1000 }, { id: 2, name: "Phone", price: 500 }, { id: 3, name: "Tablet", price: 300 }];
+//  *Input: const products = [{ id: 1, name: "Laptop", price: 1000 }, { id: 2, name: "Phone", price: 500 }, { id: 3, name: "Tablet", price: 300 }];
 //  * Với discountedPrice = price * 0.8, shippingFee = Nếu price >= 500 thì là 0, ngược lại là phí 50k
 //  * Output: [{ id: 1, discountedPrice: 800, shippingFee: 0 }, { id: 2, discountedPrice: 400, shippingFee: 0 }, { id: 3, discountedPrice: 240, shippingFee: 50 }]
 //  * 
 //  * Input: const coordinates = [
-//   { latitude: 37.783333, longitude: -122.416667 },
+//   { la2.titude: 37.783333, longitude: -122.416667 },
 //   { latitude: 40.7128, longitude: -74.0060 },
 //   { latitude: 34.0522, longitude: -118.2437 }
 //   ];
