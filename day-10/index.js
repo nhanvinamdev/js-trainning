1c0f5a4ebfdb571d9238e0e0faa0a29c62feadda
Nam Nhan <nhanvinam.dev@gmail.com>
2024-04-27 18:35:38 +0700

    push

---
+++ day-10/index.js
@@ -0,0 +1,10 @@
+// var title = document.getElementById("title");
+// var titles = document.getElementsByClassName("title");
+// const collection = document.getElementsByTagName("p");
+
+// Dấu . là classname
+// Dấu # là id
+
+const selector = document.querySelector(".box .heading:last-child");
+
+console.log(selector);
