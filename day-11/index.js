2d851306bfbdb20f0d70763a9dbdfd6619280592
Nam Nhan <nhanvinam.dev@gmail.com>
2024-04-29 18:05:47 +0700

    push

--- day-11/index.js
+++ day-11/index.js
@@ -1,3 +1,29 @@
 var headingElement = document.querySelector(".heading");
 
-headingElement.innerText = "New header";
+// II. DOM attribute
+// 1. Thêm
+
+// headingElement.id = "heading";
+// console.log(headingElement);
+// 2. Sửa: setAttribute
+headingElement.setAttribute("class", "ahihi");
+
+// 3. Lấy: getAttribute
+headingElement.getAttribute("accesskey");
+
+// III. InnerText & textContent
+// const innerText = headingElement.innerText;
+
+// const textContent = headingElement.textContent;
+
+// headingElement.innerText = "<h1>AAAAA</h1>";
+
+// console.log({
+//   innerText,
+
+//   textContent,
+// });
+
+// IV. Thêm elmemt vào element trong DOM
+// headingElement.innerHTML = "<h1>AAAAA</h1>";
+headingElement.outerHTML = "<h1>AAAAA</h1>";
